1125a1b8dcee53dfed874364422aad65aef22dd1
Pierre Prinetti <pierreprinetti@redhat.com>
2021-01-04 19:23:05 +0100

    golang: Add verify-gomod to test go mod tidy
    This patch add a new target: `verify-gomod`
    
    `verify-gomod` exits with 1 if running `go mod tidy` would change
    `go.mod` or `go.sum`. The working directory is left as if untouched.
    
    The new target is not hooked into the `verify` target; this is to avoid
    breaking existing wofkflows.

--- make/targets/golang/verify-update.mk
+++ make/targets/golang/verify-update.mk
@@ -28,6 +28,29 @@ verify-golint:
 	$(GOLINT) $(GO_PACKAGES)
 .PHONY: verify-govet
 
+verify-gomod:
+	$(info Running `$(GO) mod tidy`.)
+	@if ! [ -f go.mod ]; then \
+		echo "$@ failed - please run \`go mod init\`"; \
+		exit 1; \
+	fi; \
+	if ! [ -f go.sum ]; then \
+		echo "$@ failed - please run \`make update-gomod\`"; \
+		exit 1; \
+	fi; \
+	TMP_GOMOD=$$( mktemp ); \
+	TMP_GOSUM=$$( mktemp ); \
+	cp --preserve=all go.mod $${TMP_GOMOD}; \
+	cp --preserve=all go.sum $${TMP_GOSUM}; \
+	$(GO) mod tidy; \
+	if ! { diff $${TMP_GOMOD} go.mod && diff $${TMP_GOSUM} go.sum; }; then \
+		mv $${TMP_GOMOD} go.mod; \
+		mv $${TMP_GOSUM} go.sum; \
+		echo "$@ failed - please run \`$(GO) mod tidy\`"; \
+		exit 1; \
+	fi;
+.PHONY: verify-gomod
+
 # We need to be careful to expand all the paths before any include is done
 # or self_dir could be modified for the next include by the included file.
 # Also doing this at the end of the file allows us to use self_dir before it could be modified.
